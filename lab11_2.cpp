5ca6b0bd9eb088952ef5d55f4aa8aeda1d27dd81
Panyapat Wongdee <panyapat12345@gmail.com>
2021-12-17 16:20:46 +0700

    lab11_2.cpp completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,12 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string t;
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source, t)){
+		dest << t << "\n";
+	}
+	dest << "-------------------- HA!! ---------------------\n";
 
     source.close();
     dest.close();
